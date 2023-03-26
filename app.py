141664fb889385d78410f221cb5aa8dfe7e4f57e
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-03-26 16:11:38 +0300

    Add to db or get if it's duplicates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 
 from forms import UrlForm
 
@@ -17,11 +18,21 @@ db = SQLAlchemy(app)
 
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.Text, nullable=False)
-    url_short = db.Column(db.String(32))
+    url = db.Column(db.Text, nullable=False, unique=True)
+    url_short = db.Column(db.Integer, nullable=False, unique=True)
 
     def __repr__(self):
         return f"<Url {self.id}>"
+
+
+def get_url_short():
+    url = Url.query.order_by(desc(Url.url_short)).first()
+    if url:
+        url = url.url_short + 1
+    else:
+        url = 0
+
+    return url
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -29,7 +40,18 @@ def index():
     form = UrlForm()
 
     if form.validate_on_submit():
-        url = form.url.data
+        url_main = form.url.data
+
+        if Url.query.filter_by(url=url_main):
+            print("Url is already exist!")
+            url = Url.query.filter_by(url=url_main).first()
+            # flash("Url is already exist!")
+        else:
+            url_short = get_url_short()
+            url = Url(url=url_main, url_short=url_short)
+            db.session.add(url)
+            db.session.commit()
+
         return render_template("index.html", url=url, form=form)
     elif request.method == "POST":
         flash("Invalid url")
